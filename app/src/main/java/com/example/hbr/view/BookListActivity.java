b4a52b7a570d622771ec829db23167ee54925309
Mocsári András <mocsari.andras@autsoft.hu>
2020-03-22 15:42:51 +0100

    views created

--- app/src/main/java/com/example/hbr/view/BookListActivity.java
+++ app/src/main/java/com/example/hbr/view/BookListActivity.java
@@ -1,7 +1,9 @@
-package com.example.hbr;
+package com.example.hbr.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.hbr.R;
 
 public class BookListActivity extends AppCompatActivity {
 
