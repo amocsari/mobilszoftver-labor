454e35c0f733265d646cbef6950d4af13604a41b
Mocsári András <mocsari.andras@autsoft.hu>
2020-04-04 21:28:05 +0200

    architecture final

--- app/src/main/java/com/example/hbr/view/BookDetailActivity.java
+++ app/src/main/java/com/example/hbr/view/BookDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import com.example.hbr.HbrApplication;
 import com.example.hbr.R;
+import com.example.hbr.model.Book;
 import com.example.hbr.presenter.BookDetailPresenter;
 
 import javax.inject.Inject;
@@ -20,5 +21,31 @@ public class BookDetailActivity extends AppCompatActivity implements IBookDetail
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
         HbrApplication.injector.inject(this);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        bookDetailPresenter.attachScreen(this);
+    }
+
+    @Override
+    public Long getBookId() {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void goBack() {
+        onBackPressed();
+    }
+
+    @Override
+    public void loadBookData(Book book) {
+        throw new RuntimeException("Not implemented");
+    }
+
+    @Override
+    public void showSnackBar(String message) {
+        throw new RuntimeException("Not implemented");
     }
 }
--- app/src/main/java/com/example/hbr/view/IBookDetailView.java
+++ app/src/main/java/com/example/hbr/view/IBookDetailView.java
@@ -1,4 +1,10 @@
 package com.example.hbr.view;
 
+import com.example.hbr.model.Book;
+
 public interface IBookDetailView {
+    Long getBookId();
+    void  goBack();
+    void loadBookData(Book book);
+    void showSnackBar(String message);
 }
